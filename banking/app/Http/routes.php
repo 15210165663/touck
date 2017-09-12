55af0b3e67d9ac7db1ccb0a59b63fae702549320
xutao <2200530656@qq.com>
2017-09-12 20:49:21 +0800

    master

--- banking/app/Http/routes.php
+++ banking/app/Http/routes.php
@@ -14,56 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
-
-// Route::group(['middleware' => ['web'],'namespace' => 'Home'], function () {
-//     Route::get('project/index', ['uses' => 'ProjectController@index']);
-//     Route::get('project/elements', ['uses' => 'ProjectController@elements']);
-//     Route::get("project/portfolio", ['uses' => 'ProjectController@portfolio']);
-//     Route::get('project/blog', ['uses' => 'ProjectController@blog']);
-//     Route::get('project/blog_single', ['uses' => 'ProjectController@blog_single']);
-//     Route::get('project/contact', ['uses' => 'ProjectController@contact']);
-//     Route::get('project/balance', ['uses' => 'UserController@balance']);
-//     Route::get('project/touck', ['uses' => 'UserController@touck']);
-//     Route::get('project/sell', ['uses' => 'UserController@sell']);
-//     Route::get('project/about', ['uses' => 'UserController@about']);
-//     Route::get('project/info/{id}','ProjectController@info');
-//     Route::any('project/login', function () {
-//     	return view('home.login');
-//     });
-//     Route::get('project/register', function () {
-//     	return view('home.register');
-//     });
-//     Route::get('project/centre', function () {
-//         return view('home.centre');
-//     });
-//     Route::post('project/registers', ['uses' => 'ProjectController@registers']);
-//     Route::any('project/logins', ['uses' => 'ProjectController@logins']);
-//     //加入订单
-//     Route::post('project/indent', ['uses' => 'IndentController@indent']);
-
-
-//     Route::get('/auth/login',  'Auth\AuthController@getLogin');
-//     Route::post('/auth/login','Auth\AuthController@postLogin');
-//     Route::get('/auth/register','Auth\AuthController@getRegister');
-//     Route::post('/auth/register', 'Auth\AuthController@postRegister');
-//     Route::get('/auth/logout', 'Auth\AuthController@logout');
-
-
-// });
-
-
-
 /**
  * 前台
  */
@@ -95,7 +45,14 @@ Route::group(['middleware'=>'web','namespace' => 'Home'], function(){
         Route::match(['get','post'], '/info','FileController@info');
         //上传文件
         Route::match(['get','post'], '/ajax','loginController@ajax');
-
+        //炒股大师
+        Route::match(['get','post'], '/master','MasterController@master');
+        //炒股大师点赞
+        Route::match(['get','post'], '/dianz','MasterController@dianz');
+        //炒股大师关注
+        Route::match(['get','post'], '/guan','MasterController@guan');
+        //炒股大师详情页
+        Route::match(['get','post'], '/mastershow','MasterController@mastershow');
     });
 });
 /**
